ea1978cdf65d4b32eec0e2c713bca9f662545f22
Viktor <vikabr0524@gmail.com>
2025-03-09 21:53:59 +0100

    added more scripts for practice

---
+++ scripts/resources/[cars]/peptos_911rs/fxmanifest.lua
@@ -0,0 +1,23 @@
+fx_version 'adamant'
+game 'gta5'
+files {
+
+	'data/**/carcols.meta',
+	'data/**/carvariations.meta',
+	'data/**/handling.meta',
+	'data/**/vehiclelayouts.meta',
+	'data/**/vehicles.meta',
+        'audioconfig/tagt3flat6_game.dat151.rel',
+        'audioconfig/tagt3flat6_sounds.dat54.rel',
+        'sfx/dlc_tagt3flat6/tagt3flat6.awc',
+        'sfx/dlc_tagt3flat6/tagt3flat6_npc.awc',
+}
+data_file 'VEHICLE_LAYOUTS_FILE' 'data/**/vehiclelayouts.meta'
+data_file 'HANDLING_FILE' 'data/**/handling.meta'
+data_file 'VEHICLE_METADATA_FILE' 'data/**/vehicles.meta'
+data_file 'CARCOLS_FILE' 'data/**/carcols.meta'
+data_file 'VEHICLE_VARIATION_FILE' 'data/**/carvariations.meta'
+data_file 'AUDIO_GAMEDATA' 'audioconfig/tagt3flat6_game.dat'
+data_file 'AUDIO_SOUNDDATA' 'audioconfig/tagt3flat6_sounds.dat'
+data_file 'AUDIO_WAVEPACK' 'sfx/dlc_tagt3flat6'
+
--- scripts/resources/[cars]/sxf450sm/fxmanifest.lua
+++ scripts/resources/[cars]/sxf450sm/fxmanifest.lua
@@ -1,4 +1,7 @@
+fx_version 'cerulean'
+game 'gta5'
 resource_manifest_version '44febabe-d386-4d18-a7be-5e627f4af937'
+
 
 data_file 'VEHICLE_LAYOUTS_FILE' 'vehiclelayouts.meta'
 data_file 'CARCOLS_FILE' 'carcols.meta'
@@ -7,9 +10,8 @@ data_file 'VEHICLE_METADATA_FILE' 'vehicles.meta'
 data_file 'VEHICLE_VARIATION_FILE' 'carvariations.meta'
 
 files {
-    'vehiclelayouts.meta',
     'carcols.meta',
+    'carvariations.meta',
     'handling.meta',
     'vehicles.meta',
-    'carvariations.meta',
 }
